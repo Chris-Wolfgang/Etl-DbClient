f1e1aa2c0c1278a85bb96557d6754206fc9ee364
github-action-benchmark <github@users.noreply.github.com>
2026-05-17 02:53:13 +0000

    add ExtractorBenchmarks (postgres) (benchmarkdotnet) benchmark result for 1e3d0aa1210334485cd6a375a8fee8cf45e4d547

---
+++ dev/bench/postgres/data.js
@@ -0,0 +1,48 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1778986392744,
+  "repoUrl": "https://github.com/Chris-Wolfgang/Etl-DbClient",
+  "entries": {
+    "ExtractorBenchmarks (postgres)": [
+      {
+        "commit": {
+          "author": {
+            "name": "Chris Wolfgang",
+            "username": "Chris-Wolfgang",
+            "email": "210299580+Chris-Wolfgang@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "1e3d0aa1210334485cd6a375a8fee8cf45e4d547",
+          "message": "Merge pull request #64 from Chris-Wolfgang/feat/integration-tests-project\n\nfeat: per-RDBMS integration tests + benchmarks + README matrix",
+          "timestamp": "2026-05-17T02:25:35Z",
+          "url": "https://github.com/Chris-Wolfgang/Etl-DbClient/commit/1e3d0aa1210334485cd6a375a8fee8cf45e4d547"
+        },
+        "date": 1778986391789,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Wolfgang.Etl.DbClient.Benchmarks.ExtractorBenchmarks.ExtractAsync(RecordCount: 100)",
+            "value": 3011074.285714286,
+            "unit": "ns",
+            "range": "± 15372.466017172537"
+          },
+          {
+            "name": "Wolfgang.Etl.DbClient.Benchmarks.ExtractorBenchmarks.ExtractAsync(RecordCount: 1000)",
+            "value": 3299706.089583333,
+            "unit": "ns",
+            "range": "± 21339.212103950114"
+          },
+          {
+            "name": "Wolfgang.Etl.DbClient.Benchmarks.ExtractorBenchmarks.ExtractAsync(RecordCount: 10000)",
+            "value": 6122226.747767857,
+            "unit": "ns",
+            "range": "± 17723.809437837914"
+          }
+        ]
+      }
+    ]
+  }
+}
